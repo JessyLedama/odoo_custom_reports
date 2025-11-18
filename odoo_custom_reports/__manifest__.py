b0dfb1aef8cd5d21d5a17c4b84e2582219149bee
Jessy Ledama <jaylisteledama94@gmail.com>
2025-11-18 18:33:24 +0300

    [UPDATE]

--- odoo_custom_reports/__manifest__.py
+++ odoo_custom_reports/__manifest__.py
@@ -28,7 +28,7 @@
     'license': 'AGPL-3',
     'description': """
 """,
-    'version': '17.0.1.0',
+    'version': '18.0.1.0',
     # 'price': 59.99,
     'currency': 'EUR',
     'depends': ['base','account', 'report_xlsx'],
